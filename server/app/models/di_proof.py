5d3872f173dbc39b09ecfa885b53597501ef3123
PatStLouis <patrick.st-louis@opsecid.ca>
2025-03-07 22:50:30 +0000

    seperate di proof and di proof options
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- server/app/models/di_proof.py
+++ server/app/models/di_proof.py
@@ -42,15 +42,40 @@ class DataIntegrityProofOptions(BaseModel):
         return value
 
 
-class DataIntegrityProof(DataIntegrityProofOptions):
+class DataIntegrityProof(BaseModel):
     """DataIntegrityProof model."""
 
+    type: str = Field("DataIntegrityProof")
+    cryptosuite: str = Field("eddsa-jcs-2022")
+    proofPurpose: str = Field("assertionMethod")
     proofValue: str = Field()
     verificationMethod: str = Field()
     domain: str = Field(None)
     challenge: str = Field(None)
     created: str = Field(None)
     expires: str = Field(None)
+
+
+    @field_validator("type")
+    @classmethod
+    def validate_type(cls, value):
+        """Validate the type field."""
+        assert value == "DataIntegrityProof"
+        return value
+
+    @field_validator("cryptosuite")
+    @classmethod
+    def validate_cryptosuite(cls, value):
+        """Validate the cryptosuite field."""
+        assert value in ["eddsa-jcs-2022"]
+        return value
+
+    @field_validator("proofPurpose")
+    @classmethod
+    def validate_proof_purpose(cls, value):
+        """Validate the proofPurpose field."""
+        assert value in ["assertionMethod", "authentication"]
+        return value
 
     @field_validator("expires")
     @classmethod
