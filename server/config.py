d592767e46ff940d8820e8a0a77a74d67b90ef11
Patrick St-Louis <patrick.st-louis@opsecid.ca>
2025-10-30 09:17:59 -0400

    update charts
    Signed-off-by: Patrick St-Louis <patrick.st-louis@opsecid.ca>

--- server/config.py
+++ server/config.py
@@ -62,7 +62,7 @@ class Settings(BaseSettings):
         logging.info("Using SQLite database")
         DATABASE_URL: str = "sqlite:///app.db"
 
-    ENABLE_TAILS: bool = eval(os.environ.get("ENABLE_TAILS", "false").capitalize())
+    ENABLE_TAILS: bool = eval(os.environ.get("ENABLE_TAILS", "true").capitalize())
 
     # Recommended for production deployments
 
@@ -74,7 +74,7 @@ class Settings(BaseSettings):
     WEBVH_WATCHER: Union[str, None] = os.environ.get("WEBVH_WATCHER", None)
     WEBVH_PREROTATION: bool = eval(os.environ.get("WEBVH_PREROTATION", "true").capitalize())
     WEBVH_PORTABILITY: bool = eval(os.environ.get("WEBVH_PORTABILITY", "true").capitalize())
-    WEBVH_ENDORSEMENT: bool = eval(os.environ.get("WEBVH_ENDORSEMENT", "false").capitalize())
+    WEBVH_ENDORSEMENT: bool = eval(os.environ.get("WEBVH_ENDORSEMENT", "true").capitalize())
 
     WEBVH_ICON: str = "https://didwebvh.info/latest/assets/favicon.ico"
     WEBVH_LOGO: str = "https://raw.githubusercontent.com/decentralized-identity/didwebvh-info/main/docs/assets/didwebvh.jpg"
